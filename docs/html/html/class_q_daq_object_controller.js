fbce7cbcfab05493a4e124d5ae38f1fee49c6216
Panos Stamoulis <pstamoul@phys.uoa.gr>
2020-05-20 10:04:28 +0300

    Commit file deletions a few at a time

--- docs/html/html/class_q_daq_object_controller.js
+++
@@ -1,9 +0,0 @@
-var class_q_daq_object_controller =
-[
-    [ "QDaqObjectController", "class_q_daq_object_controller.html#ad3a5d1c00803b949a371db1156a5e823", null ],
-    [ "~QDaqObjectController", "class_q_daq_object_controller.html#ab3a80dfc4e104021dd8579503b1f05bf", null ],
-    [ "object", "class_q_daq_object_controller.html#ac90a20af3034d49cb3c32d6a2b5adf8c", null ],
-    [ "setObject", "class_q_daq_object_controller.html#a8d7772200c57429387ccb01ffc4533ce", null ],
-    [ "updateDynamicProperties", "class_q_daq_object_controller.html#a1ee9fae5a6b716b9a60c22ed8375717f", null ],
-    [ "updateProperties", "class_q_daq_object_controller.html#a8d275b2959fe034fc0426cc487546086", null ]
-];
